fc46160514eea7ed0b30acc308cd729f94e884c0
2368693074 <79479594+2368693074@users.noreply.github.com>
2021-03-08 22:48:26 +0900

    Update unicom.js

--- commands/unicom.js
+++ commands/unicom.js
@@ -1,91 +1,71 @@
-const path = require("path");
-const { scheduler } = require("../utils/scheduler");
 
-exports.command = "unicom";
+const path = require('path')
+const { buildArgs } = require('../utils/util')
+const { default: PQueue } = require('p-queue');
 
-exports.describe = "unicom任务";
+exports.command = 'unicom'
+
+exports.describe = 'unicom任务'
 
 exports.builder = function (yargs) {
   return yargs
-    .option("user", {
-      describe: "用于登录的手机号码",
-      default: "",
-      type: "string",
+    .option('user', {
+      describe: '用于登录的手机号码',
+      default: '',
+      type: 'string'
     })
-    .option("password", {
-      describe: "用于登录的账户密码",
-      default: "",
-      type: "string",
+    .option('password', {
+      describe: '用于登录的账户密码',
+      default: '',
+      type: 'string'
     })
-    .option("appid", {
-      describe: "appid",
-      default: "",
-      type: "string",
+    .option('appid', {
+      describe: 'appid',
+      default: '',
+      type: 'string'
     })
-    .option("cookies", {
-      describe: "签到cookies",
-      default: "",
-      type: "string",
+    .option('cookies', {
+      describe: '签到cookies',
+      default: '',
+      type: 'string'
     })
     .help()
-    .showHelpOnFail(true, "使用--help查看有效选项")
-    .epilog("copyright 2020 LunnLew");
-};
+    .showHelpOnFail(true, '使用--help查看有效选项')
+    .epilog('copyright 2020 LunnLew');
+}
 
 exports.handler = async function (argv) {
-  var command = argv._[0];
-  var accounts = [];
-  if ("accountSn" in argv && argv.accountSn) {
-    let accountSns = (argv.accountSn + "").split(",");
-    for (let sn of accountSns) {
-      if ("user-" + sn in argv) {
-        let account = {
-          cookies: argv["cookies-" + sn],
-          user: argv["user-" + sn] + "",
-          password: argv["password-" + sn] + "",
-          appid: argv["appid-" + sn],
-          tasks: argv["tasks-" + sn] || argv["tasks"],
-        };
-        if ("tryrun-" + sn in argv) {
-          account["tryrun"] = true;
-        }
-        accounts.push(account);
-      }
-    }
-  } else {
-    accounts.push({
-      cookies: argv["cookies"],
-      user: argv["user"] + "",
-      password: argv["password"] + "",
-      appid: argv["appid"],
-      tasks: argv["tasks"],
-    });
-  }
-  console.log("总账户数", accounts.length);
+  var command = argv._[0]
+  let accounts = buildArgs(argv)
+  console.info('总账户数', accounts.length)
+  let concurrency = 1
+  let queue = new PQueue({ concurrency });
   for (let account of accounts) {
-    await require(path.join(__dirname, "tasks", command, command))
-      .start({
+    queue.add(async () => {
+      let { scheduler } = require('../utils/scheduler')
+      await require(path.join(__dirname, 'tasks', command, command)).start({
         cookies: account.cookies,
-        options: {
-          appid: account.appid,
-          user: account.user,
-          password: account.password,
-        },
+        options: account
+      }).catch(err => console.info("unicom任务:", err))
+      let hasTasks = await scheduler.hasWillTask(command, {
+        tryrun: 'tryrun' in argv,
+        taskKey: account.user,
+        tasks: account.tasks
       })
-      .catch((err) => console.log(" unicom任务:", err));
-    let hasTasks = await scheduler.hasWillTask(command, {
-      tryrun: "tryrun" in argv,
-      taskKey: account.user,
-    });
-    if (hasTasks) {
-      scheduler
-        .execTask(command, account.tasks)
-        .catch((err) => console.log("unicom任务:", err))
-        .finally(() => {
-          console.log("当前任务执行完毕！");
-        });
-    } else {
-      console.log("暂无可执行任务！");
-    }
+      if (hasTasks) {
+        await scheduler.execTask(command, account.tasks).catch(err => console.error("unicom任务:", err)).finally(() => {
+          if (Object.prototype.toString.call(scheduler.taskJson.rewards) === '[object Object]') {
+            console.info('今日获得奖品信息统计')
+            for (let type in scheduler.taskJson.rewards) {
+              console.info(`\t`, type, scheduler.taskJson.rewards[type])
+            }
+          }
+          console.info('当前任务执行完毕！')
+        })
+      } else {
+        console.info('暂无可执行任务！')
+      }
+    })
   }
-};
+  await queue.onIdle()
+}
