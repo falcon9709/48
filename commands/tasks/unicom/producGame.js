c40d18d088a4237747f9c84e74982e69e70d2501
wuye999 <79479594+wuye999@users.noreply.github.com>
2021-03-14 19:49:41 +0900

    Update producGame.js

--- commands/tasks/unicom/producGame.js
+++ commands/tasks/unicom/producGame.js
@@ -367,13 +367,13 @@ var producGame = {
         let games = await producGame.timeTaskQuery(axios, options)
         games = allgames.filter(g => games.filter(g => g.state === '0').map(i => i.gameId).indexOf(g.id) !== -1)
         console.info('剩余未完成game', games.length)
-        let queue = new PQueue({ concurrency: 15 });
+        let queue = new PQueue({ concurrency: 1 });
 
         // 特例游戏
         // 亿万豪车2
         let others = ['1110422106']
 
-        console.info('调度任务中', '并发数', 15)
+        console.info('调度任务中', '并发数', 1)
         for (let game of games) {
             queue.add(async () => {
                 console.info(game.name)
@@ -415,9 +415,9 @@ var producGame = {
         let { games, jar } = await producGame.getTaskList(axios, options)
         games = games.filter(d => d.task === '5' && d.reachState === '0' && d.task_type === 'duration')
         console.info('剩余未完成game', games.length)
-        let queue = new PQueue({ concurrency: 15 });
+        let queue = new PQueue({ concurrency: 1 });
 
-        console.info('调度任务中', '并发数', 15)
+        console.info('调度任务中', '并发数', 1)
         for (let game of games) {
             queue.add(async () => {
                 console.info(game.name)
